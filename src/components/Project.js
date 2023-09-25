ddd14e6b68f3681cb53257456009c7f543fe0fae
ArunkumarVK <vkarunkumar446@gmail.com>
2023-09-25 19:54:58 +0530

    add course project

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,7 @@ import React from "react";
 import img1 from "../pics/img1.png";
 import img2 from "../pics/img2.png";
 import img3 from "../pics/img3.png";
+import img4 from "../pics/img4.png";
 import { FaLaptopCode } from "react-icons/fa";
 import { RiLiveFill } from "react-icons/ri";
 
@@ -14,12 +15,67 @@ export default function Project() {
             {" "}
             Project{" "}
           </h1>
+
           {/* 1 */}
-          <div className="col-lg-5 col-md-12 col-12 text-center mx-auto mt-5">
+          <div className="col-lg-5 col-md-12 col-12 text-center mx-auto">
             <br />
             <h5 className="mt-3 text-start get ">
-              Developed the front-end of the Book Store app using React JS, Node JS
-              MongoDB and ensuring a responsive and interactive user interface
+              Created a Hulu website clone using React, integrated APIs to fetch
+              and display content, and added categorized sections for an
+              immersive user experience
+            </h5>
+            <br />
+
+            <div className="mt-3">
+              <a
+                target="_blank"
+                href="https://splendorous-vacherin-c5b009.netlify.app/"
+                className="btn btn-info hand fw-bold mx-auto "
+              >
+                <h4>
+                  <RiLiveFill /> Live{" "}
+                </h4>
+              </a>
+              //{" "}
+              <a
+                target="_blank"
+                href="https://github.com/ArunkumarVK4/Course-selling-site-client"
+                className="btn btn-info  hand fw-bold my-3"
+              >
+                {" "}
+                <h4>
+                  <FaLaptopCode /> Frontend
+                </h4>
+              </a>
+              //{" "}
+              <a
+                target="_blank"
+                href="https://github.com/ArunkumarVK4/Course-selling-site-server"
+                className="btn btn-info  hand fw-bold mx-auto ml-2"
+              >
+                {" "}
+                <h4>
+                  <FaLaptopCode /> Backend
+                </h4>
+              </a>
+            </div>
+          </div>
+
+          <div className="col-lg-5 col-md-12 col-12 text-center mt-1 mx-auto mb-1">
+            <img
+              className="img-fluid mt-5 rounded-3 mb-5 border border-5 hand"
+              src={img4}
+              alt=""
+            />
+          </div>
+
+          {/* 2 */}
+          <div className="col-lg-5 col-md-12 col-12 text-center mx-auto">
+            <br />
+            <h5 className="mt-3 text-start get ">
+              Developed the front-end of the Book Store app using React JS, Node
+              JS MongoDB and ensuring a responsive and interactive user
+              interface
             </h5>
             <br />
 
@@ -29,20 +85,31 @@ export default function Project() {
                 href="https://dainty-taiyaki-f83fb9.netlify.app/"
                 className="btn btn-info hand fw-bold mx-auto "
               >
-                <h3>
-                  <RiLiveFill /> DEMO{" "}
-                </h3>
+                <h4>
+                  <RiLiveFill /> Live{" "}
+                </h4>
               </a>
               //{" "}
               <a
                 target="_blank"
                 href="https://github.com/ArunkumarVK4/Book-Store-Frontend-"
-                className="btn btn-info  hand fw-bold mx-auto"
+                className="btn btn-info  hand fw-bold my-3"
               >
                 {" "}
-                <h3>
-                  <FaLaptopCode /> CODE
-                </h3>
+                <h4>
+                  <FaLaptopCode /> Frontend
+                </h4>
+              </a>
+              //{" "}
+              <a
+                target="_blank"
+                href="https://github.com/ArunkumarVK4/Book-Store-Backend"
+                className="btn btn-info  hand fw-bold mx-auto ml-2"
+              >
+                {" "}
+                <h4>
+                  <FaLaptopCode /> Backend
+                </h4>
               </a>
             </div>
           </div>
@@ -55,37 +122,50 @@ export default function Project() {
             />
           </div>
 
-          {/* 2 */}
+          {/* 3 */}
 
-          <div className="col-lg-5 col-md-12 col-12 text-center mx-auto ">
+          <div className="col-lg-5 col-md-12 col-12 text-center mx-auto">
             <br />
             <h5 className="mt-3 text-start get ">
-              Developed a foundational MERN stack application with integrated
-              authentication, showcasing proficiency in creating secure and
-              responsive ToDo web applications.
+              Developed the front-end of the Book Store app using React JS, Node
+              JS MongoDB and ensuring a responsive and interactive user
+              interface
             </h5>
             <br />
 
             <div className="mt-3">
               <a
                 target="_blank"
-                href="https://unique-tulumba-640d05.netlify.app/"
+                href="https://unique-tulumba-640d05.netlify.app/
+                "
                 className="btn btn-info hand fw-bold mx-auto "
               >
-                <h3>
-                  <RiLiveFill /> DEMO{" "}
-                </h3>
+                <h4>
+                  <RiLiveFill /> Live{" "}
+                </h4>
               </a>
               //{" "}
               <a
                 target="_blank"
-                href="https://github.com/ArunkumarVK4/MERN-Auth-Frontend"
-                className="btn btn-info  hand fw-bold mx-auto"
+                href="
+                https://github.com/ArunkumarVK4/MERN-Auth-Frontend"
+                className="btn btn-info  hand fw-bold my-3"
               >
                 {" "}
-                <h3>
-                  <FaLaptopCode /> CODE
-                </h3>
+                <h4>
+                  <FaLaptopCode /> Frontend
+                </h4>
+              </a>
+              //{" "}
+              <a
+                target="_blank"
+                href="https://github.com/ArunkumarVK4/MERN-Auth"
+                className="btn btn-info  hand fw-bold mx-auto ml-2"
+              >
+                {" "}
+                <h4>
+                  <FaLaptopCode /> Backend
+                </h4>
               </a>
             </div>
           </div>
@@ -98,8 +178,8 @@ export default function Project() {
             />
           </div>
 
-          {/* 3 */}
-          <div className="col-lg-5 col-md-12 col-12 text-center mx-auto ">
+          {/* 4 */}
+          <div className="col-lg-5 col-md-12 col-12 text-center mx-auto">
             <br />
             <h5 className="mt-3 text-start get ">
               Created a Hulu website clone using React, integrated APIs to fetch
@@ -111,23 +191,27 @@ export default function Project() {
             <div className="mt-3">
               <a
                 target="_blank"
-                href="https://flourishing-sprite-2db1d6.netlify.app/"
+                href="https://flourishing-sprite-2db1d6.netlify.app/
+                "
                 className="btn btn-info hand fw-bold mx-auto "
               >
-                <h3>
-                  <RiLiveFill /> DEMO{" "}
-                </h3>
+                <h4>
+                  <RiLiveFill /> Live{" "}
+                </h4>
               </a>
               //{" "}
               <a
                 target="_blank"
-                href="https://github.com/ArunkumarVK4/hulu-clone"
-                className="btn btn-info  hand fw-bold mx-auto"
+                href="
+                
+                https://github.com/ArunkumarVK4/hulu-clone
+"
+                className="btn btn-info  hand fw-bold my-3"
               >
                 {" "}
-                <h3>
-                  <FaLaptopCode /> CODE
-                </h3>
+                <h4>
+                  <FaLaptopCode /> Frontend
+                </h4>
               </a>
             </div>
           </div>
